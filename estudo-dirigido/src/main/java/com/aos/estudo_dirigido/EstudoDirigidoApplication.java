e7dc75342b0f770ac02a6f8ccb7b70165e05cb5c
Helder Bernardo <142411934+heldzr@users.noreply.github.com>
2024-08-12 23:28:51 -0300

    atv 12/08

---
+++ estudo-dirigido/src/main/java/com/aos/estudo_dirigido/EstudoDirigidoApplication.java
@@ -0,0 +1,13 @@
+package com.aos.estudo_dirigido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDirigidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDirigidoApplication.class, args);
+	}
+
+}
---
+++ estudo-dirigido/src/test/java/com/aos/estudo_dirigido/EstudoDirigidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.estudo_dirigido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoDirigidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
